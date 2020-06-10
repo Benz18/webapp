9e9f4726be3540f0d4bd0488c7576f6e207cc486
Benz18 <43897766+Benz18@users.noreply.github.com>
2020-06-10 12:16:34 +0800

    Update viewresp.php

--- viewresp.php
+++ viewresp.php
@@ -1,6 +1,6 @@
 <?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "firaol", "pwd" => "{0915532504rR}", "Database" => "webapplicationDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "firaol", "pwd" => "{0915532504Rr}", "Database" => "webapplicationDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:webapplicationstp045150.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
